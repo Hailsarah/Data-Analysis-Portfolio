9be31dca69531f806e62305449d90ada6674cf55
Nabilah Sarah Azhar <84371817+Hailsarah@users.noreply.github.com>
2021-09-27 04:36:24 +0700

    Update Persebaran_Ketersediaan_Vaksin_Per-Instansi.sql

--- Data_Analysis_SQL/Covid_Vaccination_Project/Persebaran_Ketersediaan_Vaksin_Per-Instansi.sql
+++ Data_Analysis_SQL/Covid_Vaccination_Project/Persebaran_Ketersediaan_Vaksin_Per-Instansi.sql
@@ -4,4 +4,4 @@ from vaccine
 INNER JOIN instansi ON vaccine.ID_INSTANSI = instansi.ID_INSTANSI
 where vaccine.JENIS_VAKSIN is not null
 group by JML_VAKSIN_TERSEDIA, jenis_vaksin, instansi.nama_instansi
-order by JML_VAKSIN_TERSEDIA
+order by JML_VAKSIN_TERSEDIA;
