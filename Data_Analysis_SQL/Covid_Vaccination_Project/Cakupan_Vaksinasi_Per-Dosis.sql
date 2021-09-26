0a4193a238c6c266b4afb139ebc6b33e8dab0d01
Nabilah Sarah Azhar <84371817+Hailsarah@users.noreply.github.com>
2021-09-27 04:35:52 +0700

    Update Cakupan_Vaksinasi_Per-Dosis.sql

--- Data_Analysis_SQL/Covid_Vaccination_Project/Cakupan_Vaksinasi_Per-Dosis.sql
+++ Data_Analysis_SQL/Covid_Vaccination_Project/Cakupan_Vaksinasi_Per-Dosis.sql
@@ -1,6 +1,6 @@
 select dosis_vaksin, count(nik) value , concat(count(nik), ' Orang') as jumlah_orang,
 CONCAT('Dosis ', dosis_vaksin)
-from VAccination
+from Vaccination
 where DOSIS_VAKSIN is not null
 group by DOSIS_VAKSIN
 order by count(nik);
