2dd7ad93317acdcd02ea5d5f116e638219a3652c
Nabilah Sarah Azhar <84371817+Hailsarah@users.noreply.github.com>
2021-09-27 04:09:48 +0700

    Update Cakupan_Vaksin_Per-Kota.sql

--- Data_Analysis_SQL/Covid_Vaccination_Project/Cakupan_Vaksin_Per-Kota.sql
+++ Data_Analysis_SQL/Covid_Vaccination_Project/Cakupan_Vaksin_Per-Kota.sql
@@ -1,5 +1,6 @@
 --- Analyze Vaccination Coverage in Urban Area ---
 --- First, the Chart should show us: City (kota), row of citizen's id (nik), value of how many row of citizen's id (nik) + ' Orang' ---
+--- The data i took was from peserta_vaksin Table ---
 SELECT kota, COUNT(nik) value, CONCAT(COUNT(nik), ' Orang')
 FROM peserta_vaksin
 WHERE kota is not null
